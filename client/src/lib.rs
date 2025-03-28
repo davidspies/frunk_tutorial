a4d69965dd732ae783fd12bcc33f137130907f34
David Spies <dnspies@gmail.com>
2025-03-28 07:18:55 -0700

    Take receiver by value

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,10 +1,19 @@
-use generic_lib::AllFieldsPresent;
+use generic_lib::{AllFieldsPresent, AllFieldsPresentFromOwned};
 
 pub struct Foo {
     field1: Vec<i32>,
     field2: Option<char>,
     field3: Vec<String>,
     field4: Option<String>,
+}
+
+impl AllFieldsPresentFromOwned for Foo {
+    fn all_fields_present(self) -> bool {
+        !self.field1.is_empty()
+            && self.field2.is_some()
+            && !self.field3.is_empty()
+            && self.field4.is_some()
+    }
 }
 
 impl AllFieldsPresent for Foo {
--- generic_lib/src/lib.rs
+++ generic_lib/src/lib.rs
@@ -1,3 +1,7 @@
 pub trait AllFieldsPresent {
     fn all_fields_present(&self) -> bool;
 }
+
+pub trait AllFieldsPresentFromOwned {
+    fn all_fields_present(self) -> bool;
+}
