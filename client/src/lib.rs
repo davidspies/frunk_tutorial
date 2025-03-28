921c599c4eeaa7167b8cf32498d84439c9a72abe
David Spies <dnspies@gmail.com>
2025-03-28 07:28:28 -0700

    Determine all_fields_present from HList repr

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,4 @@
+use generic_lib::hlist::{HCons, HNil, h_cons};
 use generic_lib::{AllFieldsPresent, AllFieldsPresentFromOwned};
 
 pub struct Foo {
@@ -7,12 +8,21 @@ pub struct Foo {
     field4: Option<String>,
 }
 
+type FooHListRepr =
+    HCons<Vec<i32>, HCons<Option<char>, HCons<Vec<String>, HCons<Option<String>, HNil>>>>;
+
+impl Foo {
+    fn into_hlist_repr(self) -> FooHListRepr {
+        h_cons(
+            self.field1,
+            h_cons(self.field2, h_cons(self.field3, h_cons(self.field4, HNil))),
+        )
+    }
+}
+
 impl AllFieldsPresentFromOwned for Foo {
     fn all_fields_present(self) -> bool {
-        !self.field1.is_empty()
-            && self.field2.is_some()
-            && !self.field3.is_empty()
-            && self.field4.is_some()
+        self.into_hlist_repr().all_fields_present()
     }
 }
 
