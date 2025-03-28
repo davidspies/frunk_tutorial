bd1dea96ca765c0290fb8470be148521f339b80b
David Spies <dnspies@gmail.com>
2025-03-28 08:14:19 -0700

    Introduce FooRef

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -11,6 +11,25 @@ pub struct Foo {
 }
 derive_all_fields_present_from_owned!(Foo);
 
+#[derive(Generic)]
+pub struct FooRef<'a> {
+    field1: &'a Vec<i32>,
+    field2: &'a Option<char>,
+    field3: &'a Vec<String>,
+    field4: &'a Option<String>,
+}
+
+impl Foo {
+    fn to_ref<'a>(&'a self) -> FooRef<'a> {
+        FooRef {
+            field1: &self.field1,
+            field2: &self.field2,
+            field3: &self.field3,
+            field4: &self.field4,
+        }
+    }
+}
+
 impl AllFieldsPresent for Foo {
     fn all_fields_present(&self) -> bool {
         !self.field1.is_empty()
