81696c49db10590c4af63782bc6b166b07590530
David Spies <dnspies@gmail.com>
2025-03-28 08:49:43 -0700

    Extract macro

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,7 @@
 use frunk::Generic;
 use frunk_utils_derives::ToRef;
 
-use generic_lib::{AllFieldsPresent, PrefixPresent};
+use generic_lib::derive_all_fields_present;
 
 #[derive(Generic, ToRef)]
 pub struct Foo {
@@ -10,14 +10,4 @@ pub struct Foo {
     field3: Vec<String>,
     field4: Option<String>,
 }
-
-impl AllFieldsPresent for Foo {
-    fn all_fields_present(&self) -> bool {
-        use frunk::ToRef;
-        use frunk_utils::ForEach;
-
-        let mut all_fields_present = true;
-        frunk::into_generic(self.to_ref()).for_each(PrefixPresent(&mut all_fields_present));
-        all_fields_present
-    }
-}
+derive_all_fields_present!(Foo);
--- generic_lib/src/lib.rs
+++ generic_lib/src/lib.rs
@@ -1,7 +1,28 @@
+pub mod reexports {
+    pub use frunk_utils;
+}
+
 use frunk_utils::Func;
 
 pub trait AllFieldsPresent {
     fn all_fields_present(&self) -> bool;
+}
+
+#[macro_export]
+macro_rules! derive_all_fields_present {
+    ($t:ty) => {
+        impl $crate::AllFieldsPresent for $t {
+            fn all_fields_present(&self) -> bool {
+                use frunk::ToRef;
+                use $crate::reexports::frunk_utils::ForEach;
+
+                let mut all_fields_present = true;
+                frunk::into_generic(self.to_ref())
+                    .for_each($crate::PrefixPresent(&mut all_fields_present));
+                all_fields_present
+            }
+        }
+    };
 }
 
 pub struct PrefixPresent<'a>(pub &'a mut bool);
