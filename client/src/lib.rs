be2cf8ee5b34ba63fbc71c8c707d3c156ca0527b
David Spies <dnspies@gmail.com>
2025-03-28 08:58:33 -0700

    Condense a cons-list of bools

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,7 @@
 use frunk::Generic;
 use frunk_utils_derives::ToRef;
 
-use generic_lib::derive_all_fields_present;
+use generic_lib::{AllFieldsPresent, Present};
 
 #[derive(Generic, ToRef)]
 pub struct Foo {
@@ -10,4 +10,13 @@ pub struct Foo {
     field3: Vec<String>,
     field4: Option<String>,
 }
-derive_all_fields_present!(Foo);
+
+impl AllFieldsPresent for Foo {
+    fn all_fields_present(&self) -> bool {
+        use frunk::ToRef;
+        use generic_lib::reexports::frunk_utils::MapToList;
+
+        let bool_list = frunk::into_generic(self.to_ref()).map_to_list(Present);
+        bool_list.into_iter().all(|x| x)
+    }
+}
--- generic_lib/src/lib.rs
+++ generic_lib/src/lib.rs
@@ -8,37 +8,20 @@ pub trait AllFieldsPresent {
     fn all_fields_present(&self) -> bool;
 }
 
-#[macro_export]
-macro_rules! derive_all_fields_present {
-    ($t:ty) => {
-        impl $crate::AllFieldsPresent for $t {
-            fn all_fields_present(&self) -> bool {
-                use frunk::ToRef;
-                use $crate::reexports::frunk_utils::WithGeneric;
+pub struct Present;
 
-                let mut all_fields_present = true;
-                self.to_ref()
-                    .for_each($crate::PrefixPresent(&mut all_fields_present));
-                all_fields_present
-            }
-        }
-    };
-}
-
-pub struct PrefixPresent<'a>(pub &'a mut bool);
-
-impl<T> Func<&'_ Option<T>> for PrefixPresent<'_> {
-    type Output = ();
+impl<T> Func<&'_ Option<T>> for Present {
+    type Output = bool;
 
     fn call(&mut self, i: &Option<T>) -> Self::Output {
-        *self.0 &= i.is_some()
+        i.is_some()
     }
 }
 
-impl<T> Func<&'_ Vec<T>> for PrefixPresent<'_> {
-    type Output = ();
+impl<T> Func<&'_ Vec<T>> for Present {
+    type Output = bool;
 
     fn call(&mut self, i: &Vec<T>) -> Self::Output {
-        *self.0 &= !i.is_empty()
+        !i.is_empty()
     }
 }
