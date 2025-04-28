e0f93f3aeaa1c73aad678ffca938d768a20a6d56
David Spies <dnspies@gmail.com>
2025-04-27 19:04:00 -0700

    The whole shebang

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,9 +1,8 @@
-use frunk::{Generic, ToRef};
-use frunk_utils::{Func, WithGeneric};
+use frunk::Generic;
 use frunk_utils_derives::ToRef;
-use ndarray::{ArcArray, Array, ArrayView, Dimension, Ix1, Ix2, Ix3};
+use ndarray::{Ix1, Ix2, Ix3};
 
-use generic_lib::{Arcd, ArrayCarrier, Domain, Owned, Partial, View};
+use generic_lib::{Arcd, ArrayCarrier, Domain, Owned, Partial, View, impl_array_carrier};
 
 #[derive(Generic, ToRef)]
 pub struct SimulationStateG<D: Domain> {
@@ -22,61 +21,4 @@ pub type PartialSimulationState = SimulationStateG<Partial>;
 pub type SimulationStateArcs = SimulationStateG<Arcd>;
 pub type SimulationStateView<'a> = SimulationStateG<View<'a>>;
 
-impl ArrayCarrier for SimulationState {
-    type Partial = PartialSimulationState;
-    type Arcd = SimulationStateArcs;
-    type View<'a> = SimulationStateView<'a>;
-
-    fn build(partial: Self::Partial) -> Result<Self, Self::Partial> {
-        struct IsSome;
-        impl<'a, T> Func<&'a Option<T>> for IsSome {
-            type Output = bool;
-
-            fn call(&mut self, i: &'a Option<T>) -> Self::Output {
-                i.is_some()
-            }
-        }
-
-        let all_fields_present = partial.to_ref().map_to_list(IsSome).into_iter().all(|x| x);
-        if !all_fields_present {
-            return Err(partial);
-        }
-
-        struct UnwrapField;
-        impl<T> Func<Option<T>> for UnwrapField {
-            type Output = T;
-
-            fn call(&mut self, i: Option<T>) -> Self::Output {
-                i.unwrap()
-            }
-        }
-
-        Ok(partial.hmap(UnwrapField))
-    }
-
-    fn views(&self) -> Self::View<'_> {
-        struct GetView;
-        impl<'a, A, D: Dimension> Func<&'a Array<A, D>> for GetView {
-            type Output = ArrayView<'a, A, D>;
-
-            fn call(&mut self, i: &'a Array<A, D>) -> Self::Output {
-                i.view()
-            }
-        }
-
-        self.to_ref().hmap(GetView)
-    }
-
-    fn arcs(self) -> Self::Arcd {
-        struct ArcArrayFrom;
-        impl<A, D: Dimension> Func<Array<A, D>> for ArcArrayFrom {
-            type Output = ArcArray<A, D>;
-
-            fn call(&mut self, i: Array<A, D>) -> Self::Output {
-                ArcArray::from(i)
-            }
-        }
-
-        self.hmap(ArcArrayFrom)
-    }
-}
+impl_array_carrier!(SimulationStateG);
--- generic_lib/src/lib.rs
+++ generic_lib/src/lib.rs
@@ -1,6 +1,12 @@
 use std::marker::PhantomData;
 
-use ndarray::{ArcArray, Array, ArrayView};
+use frunk_utils::Func;
+use ndarray::{ArcArray, Array, ArrayView, Dimension};
+
+pub mod reexport {
+    pub use frunk;
+    pub use frunk_utils;
+}
 
 pub trait Domain {
     type Array<DType: 'static, Idx>;
@@ -38,4 +44,77 @@ pub trait ArrayCarrier {
         Self: Sized;
     fn views(&self) -> Self::View<'_>;
     fn arcs(self) -> Self::Arcd;
+}
+
+pub struct IsSome;
+impl<'a, T> Func<&'a Option<T>> for IsSome {
+    type Output = bool;
+
+    fn call(&mut self, i: &'a Option<T>) -> Self::Output {
+        i.is_some()
+    }
+}
+
+pub struct UnwrapField;
+impl<T> Func<Option<T>> for UnwrapField {
+    type Output = T;
+
+    fn call(&mut self, i: Option<T>) -> Self::Output {
+        i.unwrap()
+    }
+}
+
+pub struct GetView;
+impl<'a, A, D: Dimension> Func<&'a Array<A, D>> for GetView {
+    type Output = ArrayView<'a, A, D>;
+
+    fn call(&mut self, i: &'a Array<A, D>) -> Self::Output {
+        i.view()
+    }
+}
+
+pub struct ArcArrayFrom;
+impl<A, D: Dimension> Func<Array<A, D>> for ArcArrayFrom {
+    type Output = ArcArray<A, D>;
+
+    fn call(&mut self, i: Array<A, D>) -> Self::Output {
+        ArcArray::from(i)
+    }
+}
+
+#[macro_export]
+macro_rules! impl_array_carrier {
+    ($name:ident) => {
+        impl ArrayCarrier for $name<$crate::Owned> {
+            type Partial = $name<$crate::Partial>;
+            type Arcd = $name<$crate::Arcd>;
+            type View<'a> = $name<$crate::View<'a>>;
+
+            fn build(partial: Self::Partial) -> Result<Self, Self::Partial> {
+                use $crate::reexport::frunk::ToRef;
+                use $crate::reexport::frunk_utils::WithGeneric;
+                let all_fields_present = partial
+                    .to_ref()
+                    .map_to_list($crate::IsSome)
+                    .into_iter()
+                    .all(|x| x);
+                if all_fields_present {
+                    Ok(partial.hmap($crate::UnwrapField))
+                } else {
+                    Err(partial)
+                }
+            }
+
+            fn views(&self) -> Self::View<'_> {
+                use $crate::reexport::frunk::ToRef;
+                use $crate::reexport::frunk_utils::WithGeneric;
+                self.to_ref().hmap($crate::GetView)
+            }
+
+            fn arcs(self) -> Self::Arcd {
+                use $crate::reexport::frunk_utils::WithGeneric;
+                self.hmap($crate::ArcArrayFrom)
+            }
+        }
+    };
 }
