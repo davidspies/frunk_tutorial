6f17da2ca85a90e61eb1a8d7f9333f8923817f42
David Spies <david.spies@matician.com>
2025-05-09 17:16:58 -0700

    Explicit dimension parameters

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,50 +1,48 @@
 use generic_lib::ArrayFields;
-use ndarray::{
-    ArcArray, ArcArray1, ArcArray2, Array1, Array2, Array3, ArrayView1, ArrayView2, ArrayView3, Ix3,
-};
+use ndarray::{ArcArray, Array, ArrayView, Ix1, Ix2, Ix3};
 
 pub struct SimulationState {
-    pub positions: Array2<f64>,
-    pub velocities: Array2<f64>,
-    pub particle_types: Array1<i32>,
-    pub is_active_mask: Array1<bool>,
-    pub density_field: Array3<f32>,
-    pub event_timestamps: Array1<i64>,
-    pub connectivity_matrix: Array2<u8>,
-    pub sensor_readings: Array2<f32>,
+    pub positions: Array<f64, Ix2>,
+    pub velocities: Array<f64, Ix2>,
+    pub particle_types: Array<i32, Ix1>,
+    pub is_active_mask: Array<bool, Ix1>,
+    pub density_field: Array<f32, Ix3>,
+    pub event_timestamps: Array<i64, Ix1>,
+    pub connectivity_matrix: Array<u8, Ix2>,
+    pub sensor_readings: Array<f32, Ix2>,
 }
 
 pub struct PartialSimulationState {
-    pub positions: Option<Array2<f64>>,
-    pub velocities: Option<Array2<f64>>,
-    pub particle_types: Option<Array1<i32>>,
-    pub is_active_mask: Option<Array1<bool>>,
-    pub density_field: Option<Array3<f32>>,
-    pub event_timestamps: Option<Array1<i64>>,
-    pub connectivity_matrix: Option<Array2<u8>>,
-    pub sensor_readings: Option<Array2<f32>>,
+    pub positions: Option<Array<f64, Ix2>>,
+    pub velocities: Option<Array<f64, Ix2>>,
+    pub particle_types: Option<Array<i32, Ix1>>,
+    pub is_active_mask: Option<Array<bool, Ix1>>,
+    pub density_field: Option<Array<f32, Ix3>>,
+    pub event_timestamps: Option<Array<i64, Ix1>>,
+    pub connectivity_matrix: Option<Array<u8, Ix2>>,
+    pub sensor_readings: Option<Array<f32, Ix2>>,
 }
 
 pub struct SimulationStateArcs {
-    pub positions: ArcArray2<f64>,
-    pub velocities: ArcArray2<f64>,
-    pub particle_types: ArcArray1<i32>,
-    pub is_active_mask: ArcArray1<bool>,
+    pub positions: ArcArray<f64, Ix2>,
+    pub velocities: ArcArray<f64, Ix2>,
+    pub particle_types: ArcArray<i32, Ix1>,
+    pub is_active_mask: ArcArray<bool, Ix1>,
     pub density_field: ArcArray<f32, Ix3>,
-    pub event_timestamps: ArcArray1<i64>,
-    pub connectivity_matrix: ArcArray2<u8>,
-    pub sensor_readings: ArcArray2<f32>,
+    pub event_timestamps: ArcArray<i64, Ix1>,
+    pub connectivity_matrix: ArcArray<u8, Ix2>,
+    pub sensor_readings: ArcArray<f32, Ix2>,
 }
 
 pub struct SimulationStateView<'a> {
-    pub positions: ArrayView2<'a, f64>,
-    pub velocities: ArrayView2<'a, f64>,
-    pub particle_types: ArrayView1<'a, i32>,
-    pub is_active_mask: ArrayView1<'a, bool>,
-    pub density_field: ArrayView3<'a, f32>,
-    pub event_timestamps: ArrayView1<'a, i64>,
-    pub connectivity_matrix: ArrayView2<'a, u8>,
-    pub sensor_readings: ArrayView2<'a, f32>,
+    pub positions: ArrayView<'a, f64, Ix2>,
+    pub velocities: ArrayView<'a, f64, Ix2>,
+    pub particle_types: ArrayView<'a, i32, Ix1>,
+    pub is_active_mask: ArrayView<'a, bool, Ix1>,
+    pub density_field: ArrayView<'a, f32, Ix3>,
+    pub event_timestamps: ArrayView<'a, i64, Ix1>,
+    pub connectivity_matrix: ArrayView<'a, u8, Ix2>,
+    pub sensor_readings: ArrayView<'a, f32, Ix2>,
 }
 
 impl PartialSimulationState {
